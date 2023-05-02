b48f538cf34bdc4113cc8309b28c9c32160c79aa
Sebastián Petrík <plasmoxy@gmail.com>
2023-05-02 11:40:29 +0200

    single output image

--- src/web/mod.rs
+++ src/web/mod.rs
@@ -2,12 +2,13 @@ pub mod routes;
 pub mod util;
 
 use self::routes::*;
-use axum::{routing::post, Router};
 use crate::neural::NeuralInferrer;
+use axum::{routing::post, Router};
 
 pub fn routes(inferrer: NeuralInferrer) -> Router {
-	Router::new()
+    Router::new()
         .route("/detect", post(detect))
+        .route("/detect-single", post(detect_single))
         .route("/distort", post(distort))
         .route("/invert", post(invert))
         .route("/trim", post(trim))
--- src/web/routes.rs
+++ src/web/routes.rs
@@ -1,18 +1,30 @@
-use axum::{extract::{Multipart, State, Path}, Json};
+use axum::{
+    extract::{Multipart, Path, State},
+    response::{IntoResponse, Response},
+    Json,
+};
 use axum_macros::debug_handler;
-use image::{imageops::resize, ImageBuffer, Rgb};
-use std::time::Instant;
+use image::{imageops::resize, ImageBuffer, ImageFormat, Rgb};
+use reqwest::{header::CONTENT_TYPE, StatusCode};
+use std::{
+    io::{BufWriter, Cursor},
+    time::Instant,
+};
 
-use crate::{neural::NeuralInferrer, images::processing::Processing, web::util::{ImageResponse, parse_u32, CropImage}};
-use crate::images::{draw_bboxes_on_image, load_image_from_bytes, get_image_as_bytes};
+use crate::images::{draw_bboxes_on_image, get_image_as_bytes, load_image_from_bytes};
+use crate::{
+    images::processing::Processing,
+    neural::NeuralInferrer,
+    web::util::{parse_u32, CropImage, ImageResponse},
+};
 
 #[debug_handler]
 pub async fn detect(State(inferrer): State<NeuralInferrer>, mut data: Multipart) -> Json<Vec<ImageResponse>> {
     let mut response: Vec<ImageResponse> = Vec::new();
-    
+
     while let Some(field) = data.next_field().await.unwrap() {
         let (name, buf) = load_image_from_bytes(field).await.unwrap();
-        
+
         let bboxes = inferrer.infer_face(&buf);
         let detected = draw_bboxes_on_image(
             resize(&buf, buf.width() / 4, buf.height() / 4, image::imageops::FilterType::Triangle),
@@ -20,7 +32,7 @@ pub async fn detect(State(inferrer): State<NeuralInferrer>, mut data: Multipart)
         );
 
         let bytes = get_image_as_bytes(detected).unwrap();
-    
+
         response.push(ImageResponse::new(name, bytes));
     }
 
@@ -28,18 +40,38 @@ pub async fn detect(State(inferrer): State<NeuralInferrer>, mut data: Multipart)
 }
 
 #[debug_handler]
+pub async fn detect_single(State(inferrer): State<NeuralInferrer>, mut data: Multipart) -> Response {
+    if let Some(field) = data.next_field().await.unwrap() {
+        let (name, buf) = load_image_from_bytes(field).await.unwrap();
+
+        let bboxes = inferrer.infer_face(&buf);
+        let detected = draw_bboxes_on_image(
+            resize(&buf, buf.width() / 4, buf.height() / 4, image::imageops::FilterType::Triangle),
+            bboxes,
+        );
+
+        let mut buffer = BufWriter::new(Cursor::new(Vec::new()));
+        detected.write_to(&mut buffer, ImageFormat::Jpeg).unwrap();
+        let bytes: Vec<u8> = buffer.into_inner().unwrap().into_inner();
+        return (StatusCode::OK, [(CONTENT_TYPE, "image/png")], bytes).into_response();
+    }
+
+    (StatusCode::BAD_REQUEST).into_response()
+}
+
+#[debug_handler]
 pub async fn distort(mut data: Multipart) -> Json<Vec<ImageResponse>> {
     let mut response: Vec<ImageResponse> = Vec::new();
-    
+
     while let Some(field) = data.next_field().await.unwrap() {
-        let (name, mut buf) = load_image_from_bytes(field).await.unwrap();    
+        let (name, mut buf) = load_image_from_bytes(field).await.unwrap();
 
         let time = Instant::now();
         Processing::wobble(&mut buf);
         println!("Wobble of {} finished in {:?}", name, time.elapsed());
 
         let bytes = get_image_as_bytes(buf).unwrap();
-    
+
         response.push(ImageResponse::new(name, bytes));
     }
 
@@ -49,16 +81,16 @@ pub async fn distort(mut data: Multipart) -> Json<Vec<ImageResponse>> {
 #[debug_handler]
 pub async fn invert(mut data: Multipart) -> Json<Vec<ImageResponse>> {
     let mut response: Vec<ImageResponse> = Vec::new();
-    
+
     while let Some(field) = data.next_field().await.unwrap() {
-        let (name, mut buf) = load_image_from_bytes(field).await.unwrap();    
+        let (name, mut buf) = load_image_from_bytes(field).await.unwrap();
 
         let time = Instant::now();
         Processing::negative_basic(&mut buf);
         println!("Inversion of {} finished in {:?}", name, time.elapsed());
 
         let bytes = get_image_as_bytes(buf).unwrap();
-    
+
         response.push(ImageResponse::new(name, bytes));
     }
 
@@ -68,16 +100,16 @@ pub async fn invert(mut data: Multipart) -> Json<Vec<ImageResponse>> {
 #[debug_handler]
 pub async fn trim(mut data: Multipart) -> Json<Vec<ImageResponse>> {
     let mut response: Vec<ImageResponse> = Vec::new();
-    
+
     while let Some(field) = data.next_field().await.unwrap() {
-        let (name, mut buf) = load_image_from_bytes(field).await.unwrap();    
+        let (name, mut buf) = load_image_from_bytes(field).await.unwrap();
 
         let time = Instant::now();
         buf = Processing::remove_borders(&buf).unwrap();
         println!("Removed black borders from {} in {:?}", name, time.elapsed());
 
         let bytes = get_image_as_bytes(buf).unwrap();
-    
+
         response.push(ImageResponse::new(name, bytes));
     }
 
@@ -87,16 +119,16 @@ pub async fn trim(mut data: Multipart) -> Json<Vec<ImageResponse>> {
 #[debug_handler]
 pub async fn rotate(Path(angle): Path<f32>, mut data: Multipart) -> Json<Vec<ImageResponse>> {
     let mut response: Vec<ImageResponse> = Vec::new();
- 
+
     while let Some(field) = data.next_field().await.unwrap() {
-        let (name, mut buf) = load_image_from_bytes(field).await.unwrap();    
+        let (name, mut buf) = load_image_from_bytes(field).await.unwrap();
 
         let time = Instant::now();
         buf = Processing::rotate(&buf, angle);
         println!("Rotation of {} finished in {:?}", name, time.elapsed());
 
         let bytes = get_image_as_bytes(buf).unwrap();
-    
+
         response.push(ImageResponse::new(name, bytes));
     }
 
@@ -110,22 +142,33 @@ pub async fn crop(mut data: Multipart) -> Json<Vec<ImageResponse>> {
     let mut args = CropImage::new();
     let mut file_name: Option<String> = None;
     let mut buffer: Option<ImageBuffer<Rgb<u8>, Vec<u8>>> = None;
-    
+
     while let Some(field) = data.next_field().await.unwrap() {
-        
         let name = field.name().unwrap().to_string();
 
         match name.as_str() {
-            "x" => { args.x = Some(parse_u32(field).await.unwrap()); continue },
-            "y" => { args.y = Some(parse_u32(field).await.unwrap()); continue },
-            "width" => { args.width = Some(parse_u32(field).await.unwrap()); continue },
-            "height" => { args.height = Some(parse_u32(field).await.unwrap()); continue },
-            "image" => {        
+            "x" => {
+                args.x = Some(parse_u32(field).await.unwrap());
+                continue;
+            }
+            "y" => {
+                args.y = Some(parse_u32(field).await.unwrap());
+                continue;
+            }
+            "width" => {
+                args.width = Some(parse_u32(field).await.unwrap());
+                continue;
+            }
+            "height" => {
+                args.height = Some(parse_u32(field).await.unwrap());
+                continue;
+            }
+            "image" => {
                 let (name, buf) = load_image_from_bytes(field).await.unwrap();
                 file_name = Some(name);
-                buffer = Some(buf); 
+                buffer = Some(buf);
             }
-            _ => ()
+            _ => (),
         };
     }
 
@@ -138,7 +181,7 @@ pub async fn crop(mut data: Multipart) -> Json<Vec<ImageResponse>> {
 
         println!("Cropping of {} finished in {:?}", name, time.elapsed());
         let bytes = get_image_as_bytes(buf).unwrap();
-    
+
         response.push(ImageResponse::new(name, bytes));
     }
 
